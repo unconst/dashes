7c4b17a46e92c28f4f485041561cb5b8e4547008
unconst <jake@bittensor.com>
2023-06-01 15:36:04 -0700

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ for i in range(100):
 
         # Build items.
         col1, col2, col3, col4 = st.columns(4)
-        block_metric = col1.metric( label="Block", value=str(history[-1].block.item()), delta = str(history[-1].block.item() - history[-2].block.item()) )
-        block_metric = col2.metric( label="UIDs", value=str(history[-1].n.item()), delta = str(history[-1].n.item() - history[-2].n.item()) )
-        block_metric = col3.metric( label="Unique Hotkeys", value=str(len(set(history[-1].hotkeys))), delta = str(len(set(history[-1].hotkeys))) - len(set(history[-2].hotkeys))) 
-        block_metric = col4.metric( label="Unique Coldkeys", value=str(len(set(history[-1].coldkeys))), delta = str(len(set(history[-1].coldkeys))) - len(set(history[-2].coldkeys))) 
-        block_metric = col1.metric( label="Unique Endpoints", value=str(len(set([a.ip for a in history[-1].axons]))), delta = str(len(set([a.ip for a in history[-1].axons]))) )
+        block_metric = col1.metric( label="Block", value = history[-1].block.item(), delta = history[-1].block.item() - history[-2].block.item() )
+        block_metric = col2.metric( label="UIDs", value = history[-1].n.item(), delta = history[-1].n.item() - history[-2].n.item() )
+        block_metric = col3.metric( label="Unique Hotkeys", value = len(set(history[-1].hotkeys)), delta = len(set(history[-1].hotkeys)) - len(set(history[-2].hotkeys) ) )
+        block_metric = col4.metric( label="Unique Coldkeys", value = len(set(history[-1].coldkeys)), delta = len(set(history[-1].coldkeys)) - len(set(history[-2].coldkeys)) ) 
+        block_metric = col1.metric( label="Unique Endpoints", value = len(set([a.ip for a in history[-1].axons])), delta = len(set([a.ip for a in history[-1].axons])) )
 
         # Build table.
         arr = np.random.normal(1, 1, size=100)
