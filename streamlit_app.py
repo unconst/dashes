1765b8ff86b9eb6ac550b7cc5980f5cec056ccf8
unconst <jake@bittensor.com>
2023-06-01 15:07:50 -0700

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import time
 import streamlit as st
 import bittensor as bt
 
+st.set_page_config(
+    page_title="Opentensor Dashboard",
+    page_icon="𝜏",
+    layout="wide",
+)
+
 sub = bt.subtensor()
 st.title("Bittensor: Finney")
 st.divider()
@@ -9,8 +15,8 @@ st.divider()
 # Block metric.
 block_metric = st.metric( label="Block", value=str(sub.get_current_block()) )
 
-# Streamlit loop.
-for i in range(100):
-    time.sleep(1)
-    sub = bt.subtensor()
-    block_metric.value = str( sub.get_current_block() )
+# # Streamlit loop.
+# for i in range(100):
+#     time.sleep(1)
+#     sub = bt.subtensor()
+#     block_metric.value = str( sub.get_current_block() )
