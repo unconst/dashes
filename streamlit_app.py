6d0d2e6451273bdc097035453c67787d922989f6
unconst <jake@bittensor.com>
2023-06-01 15:00:25 -0700

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,14 @@ import streamlit as st
 import bittensor as bt
 
 sub = bt.subtensor()
-st_block = st.markdown("Block: {}".format( sub.get_current_block() ) )
-for i in range(100):
-    time.sleep(1)
-    sub = bt.subtensor()
-    st_block.write( "Block: {}".format( sub.get_current_block() ) )
+st.title("Bittensor: Finney")
+st.divider()
+st.metric( label="Block", value=str(sub.get_current_block()) )
+
+
+
+# st_block = st.markdown("Block: {}".format( sub.get_current_block() ) )
+# for i in range(100):
+#     time.sleep(1)
+#     sub = bt.subtensor()
+#     st_block.write( "Block: {}".format( sub.get_current_block() ) )
