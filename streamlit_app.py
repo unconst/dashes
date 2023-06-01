9544754fa33fcfdbcbf25c77d81fbd78f5634e63
unconst <jake@bittensor.com>
2023-06-01 14:53:40 -0700

    add favicon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import time
 import streamlit as st
 import bittensor as bt
+from PIL import Image
+
+# Set up page.
+image = Image.open('assets/favicon.png')
+st.image(image, caption='Page favicon')
+st.set_page_config(
+    page_title = "Opentensor Dashboards",
+    page_icon = st.image(image, caption='Page favicon'),
+    layout = "wide",
+)
+
 for i in range(100):
     time.sleep(1)
     sub = bt.subtensor()
