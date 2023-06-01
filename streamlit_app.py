3b3e70388fe2dedebea9bc74cb468715510b2ac7
unconst <jake@bittensor.com>
2023-06-01 15:11:55 -0700

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,14 @@ sub = bt.subtensor()
 st.title("Bittensor: Finney")
 st.divider()
 
-# Block metric.
-block_metric = st.metric( label="Block", value=str(sub.get_current_block()) )
+# Streamlit loop.
+placeholder = st.empty()
 
-# # Streamlit loop.
-# for i in range(100):
+for i in range(100):
+    with placeholder.container():
+        block_metric = st.metric( label="Block", value=str(sub.get_current_block()) )
+        time.sleep(1)
+
 #     time.sleep(1)
 #     sub = bt.subtensor()
 #     block_metric.value = str( sub.get_current_block() )
