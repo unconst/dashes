1389b2941f3cc54d0d98bbf21bfd7364c0657d06
unconst <jake@bittensor.com>
2023-06-01 14:02:54 -0700

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import time
 import streamlit as st
+import bittensor as bt
 for i in range(100):
     time.sleep(1)
-    st.markdown("# Value of i is: {}".format( i ) )
+    sub = bt.subtensor()
+    st.markdown("# Value of i is: {}".format( sub.get_current_block() ) )
