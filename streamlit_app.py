8a803b4c618d93be39bde0f5dbe578aa58389d35
unconst <jake@bittensor.com>
2023-06-01 14:57:45 -0700

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,10 @@
 import time
 import streamlit as st
 import bittensor as bt
-from PIL import Image
 
-# Set up page.
-image = Image.open('assets/favicon.ico')
-st.image(image, caption='Page favicon')
-st.set_page_config(
-    page_title = "Opentensor Dashboards",
-    page_icon = st.image(image, caption='Page favicon'),
-    layout = "wide",
-)
-
+sub = bt.subtensor()
+st_block = st.markdown("Block: {}".format( sub.get_current_block() ) )
 for i in range(100):
     time.sleep(1)
     sub = bt.subtensor()
-    st.markdown("# Value of i is: {}".format( sub.get_current_block() ) )
+    st_block.write( "Block: {}".format( sub.get_current_block() ) )
